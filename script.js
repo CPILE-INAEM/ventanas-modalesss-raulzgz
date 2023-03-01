c2190c9991569245dfd162b03621fb6e5abc68db
Raul_Navarro_Sos <raulnsos@gmail.com>
2023-03-01 13:39:19 +0100

    Cerrar ventana
    Añadido el codigo para cerrar la ventana

--- script.js
+++ script.js
@@ -3,14 +3,17 @@ const modal = document.querySelector('.modal')
 const btnCloseModal = document.querySelector('.close-modal')
 const overlay = document.querySelector('.overlay')
 
-console.log(btnShowModal)
-console.log(modal)
-console.log(btnCloseModal)
-console.log(overlay)
-
 const openModal = function () {
   modal.classList.remove('hidden')
   overlay.classList.remove('hidden')
 }
 
+const closeModal = function () {
+  modal.classList.add('hidden')
+  overlay.classList.add('hidden')
+}
+
 btnShowModal.forEach((btn) => btn.addEventListener('click', openModal))
+
+overlay.addEventListener('click', closeModal)
+btnCloseModal.addEventListener('click', closeModal)
