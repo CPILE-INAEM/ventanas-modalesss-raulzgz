5e4ed0ff430c25e44a9e10f4a81bca9e3009083d
Raul_Navarro_Sos <raulnsos@gmail.com>
2023-03-01 14:08:10 +0100

    Tecla Escape
    Implementan de la función añadida cerrar ventana emergente con escape.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const btnShowModal = document.querySelectorAll('.show-modal')
+//const btnShowModal = document.querySelectorAll('.show-modal')
+const btnShowModal = document.querySelectorAll('#botones')
 const modal = document.querySelector('.modal')
 const btnCloseModal = document.querySelector('.close-modal')
 const overlay = document.querySelector('.overlay')
@@ -17,3 +18,10 @@ btnShowModal.forEach((btn) => btn.addEventListener('click', openModal))
 
 overlay.addEventListener('click', closeModal)
 btnCloseModal.addEventListener('click', closeModal)
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal()
+  }
+  e.stopPropagation()
+})
